6f6fa59323ea4ecf278d51555a25f5df60a56fce
Anton Maminov <anton.linux@gmail.com>
2025-02-21 21:07:11 +0200

    avg_speed_in_mbps -> speed_in_mbps

--- src/speedtest-cli.cr
+++ src/speedtest-cli.cr
@@ -171,7 +171,7 @@ module Speedtest
     end_time = Time.monotonic
     total_time = end_time - start_time
 
-    puts "🔼 Download: #{avg_speed_in_mbps(total_bytes.get, total_time)}"
+    puts "🔼 Download: #{speed_in_mbps(total_bytes.get, total_time)}"
   end
 
   def test_upload_speed(host : String, config : Config, single_mode : Bool)
@@ -221,14 +221,14 @@ module Speedtest
     end_time = Time.monotonic
     total_time = end_time - start_time
 
-    puts "🔼 Upload: #{avg_speed_in_mbps(total_bytes.get, total_time)}"
+    puts "🔼 Upload: #{speed_in_mbps(total_bytes.get, total_time)}"
   end
 
   def update_progress_bar(start_time : Time::Span, total_bytes : Int64, completed_requests : Int32, total_requests : Int32)
     sleep 100.milliseconds
     elapsed_time = Time.monotonic - start_time
 
-    speed_mbps = avg_speed_in_mbps(total_bytes, elapsed_time)
+    speed_mbps = speed_in_mbps(total_bytes, elapsed_time)
 
     percentage = ((completed_requests / total_requests) * 100).clamp(0, 100).to_i
     bar_length = (percentage / 2).to_i
@@ -238,7 +238,7 @@ module Speedtest
     STDOUT.flush
   end
 
-  private def avg_speed_in_mbps(bytes : Int64, elapsed_time : Time::Span) : String
+  private def speed_in_mbps(bytes : Int64, elapsed_time : Time::Span) : String
     avg_speed = (bytes * 8) / (elapsed_time.total_seconds * 1_000_000.0)
 
     "#{avg_speed.round(2)} Mbit/s"
