99ca5547e4ef3ded4510f8f1b37ddbaea37cd5d5
Anton Maminov <anton.linux@gmail.com>
2025-02-24 11:22:43 +0200

    comment specs

--- spec/speedtest-cli_spec.cr
+++ spec/speedtest-cli_spec.cr
@@ -1,9 +1,7 @@
 require "./spec_helper"
 
-describe Speedtest::Cli do
-  # TODO: Write tests
-
-  it "works" do
-    false.should eq(true)
-  end
+describe Speedtest::CLI do
+  # it "works" do
+  #   false.should eq(true)
+  # end
 end
